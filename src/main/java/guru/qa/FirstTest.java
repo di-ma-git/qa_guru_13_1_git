628e3e3a5722a0a6f3a7cfdd4fc825ce7ac3a933
Dima Maltsev <phoenixnety@gmail.com>
2022-06-08 22:04:55 +0300

    first commit

---
+++ src/main/java/guru/qa/FirstTest.java
@@ -0,0 +1,13 @@
+package guru.qa;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.Assertion;
+
+public class FirstTest {
+
+    @Test
+    void firstTest{
+        Assertion.assertTrue(3 > 2)
+
+    }
+}
